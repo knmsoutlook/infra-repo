7823fc387ee92faada634acb5107090b2faa7539
Kamal <40554278+knmsoutlook@users.noreply.github.com>
2026-01-10 22:52:33 -0600

    vpc_security

--- main.tf
+++ main.tf
@@ -18,15 +18,16 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
-resource "aws_security_group" "app_sg" {
-  name   = "app-sg-unique"
+data "aws_security_group" "app_sg" {
+  name   = "app-sg"
   vpc_id = data.aws_vpc.default.id
 }
 resource "aws_instance" "app" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  #vpc_security_group_ids = [aws_security_group.app_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.app_sg.id]
   key_name               = var.key_name
 
   user_data = <<-EOF
