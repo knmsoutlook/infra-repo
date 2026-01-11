6fc924a1738ec99f1b9de2550a1543557e9bac77
Kamal <40554278+knmsoutlook@users.noreply.github.com>
2026-01-10 22:57:08 -0600

    main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+# main.tf
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Use the default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
+# Get default subnets
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
@@ -9,6 +17,7 @@ data "aws_subnets" "default" {
   }
 }
 
+# Get the latest Ubuntu 22.04 AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
@@ -18,15 +27,18 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
+
+# Use the existing security group
 data "aws_security_group" "app_sg" {
   name   = "app-sg"
   vpc_id = data.aws_vpc.default.id
 }
+
+# EC2 instance
 resource "aws_instance" "app" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = data.aws_subnets.default.ids[0]
-  #vpc_security_group_ids = [aws_security_group.app_sg.id]
   vpc_security_group_ids = [data.aws_security_group.app_sg.id]
   key_name               = var.key_name
 
